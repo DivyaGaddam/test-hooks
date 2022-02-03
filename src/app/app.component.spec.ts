f77186a327aba24c1078e32228643238041a1dff
Divya Gaddam <dgaddam@nettracer.aero>
2022-02-03 12:50:31 +0530

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'test-hooks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-ho');
+    expect(app.title).toEqual('test-hooks');
   });
 
   it('should render title', () => {
